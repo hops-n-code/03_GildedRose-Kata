b9755c9e69fb8bdbf98084ae091e3a70983bcc83
Cesar Romero <hops.n.code@gmail.com>
2022-08-27 20:50:46 -0500

    Initial commit

---
+++ src/test/java/com/hops_n_code/dojo/PackageSettings.java
@@ -0,0 +1,7 @@
+package com.hops_n_code.dojo;
+
+import org.approvaltests.reporters.JunitReporter;
+
+public class PackageSettings {
+    public static JunitReporter UseReporter = JunitReporter.INSTANCE;
+}
