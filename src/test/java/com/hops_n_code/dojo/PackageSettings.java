6cedf82e8db181f9cc66bc6ca2520f0b68f4f2c3
Cesar Romero <hops.n.code@gmail.com>
2025-06-19 03:05:59 -0600

    Delete unnecessary file
    Use @UseReporter(JunitReporter.class) instead

--- src/test/java/com/hops_n_code/dojo/PackageSettings.java
+++
@@ -1,7 +0,0 @@
-package com.hops_n_code.dojo;
-
-import org.approvaltests.reporters.JunitReporter;
-
-public class PackageSettings {
-    public static JunitReporter UseReporter = JunitReporter.INSTANCE;
-}
